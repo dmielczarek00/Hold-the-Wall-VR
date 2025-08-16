8121bcbfebdf2e6374fe2513518ee267a3e0e56d
dmielczarek00 <122283284+dmielczarek00@users.noreply.github.com>
2025-08-16 23:30:54 +0200

    Added menu

---
+++ Assets/Scripts/MainMenu/MenuManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject mainPanel;
+    [SerializeField] private GameObject optionsPanel;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void OpenOptions(bool open)
+    {
+        if (optionsPanel) optionsPanel.SetActive(open);
+        if (mainPanel) mainPanel.SetActive(!open);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
